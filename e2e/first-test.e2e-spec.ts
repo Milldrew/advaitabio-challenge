0928b6dee83987ffe59d486c831c343173b3dcdd
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-05 16:18:06 -0600

    📅  | Fri Aug  5 16:18:06 MDT 2022

--- e2e/first-test.e2e-spec.ts
+++ e2e/first-test.e2e-spec.ts
@@ -6,23 +6,20 @@ describe('our first Protractor test', () => {
     await browser.get('');
     await browser.waitForAngular();
   });
-  /*
   it('App is runningn on localhost:4200', async () => {
     expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
     const title = await browser.getTitle();
   });
   it('The plot graph instantiates', async () => {
     //const h1Ele = await element(by.name('h1')).getText();
-    //console.log(h1Ele);
     const svgElement = element(by.tagName('svg'));
     const svgIsPresent = await svgElement.isPresent();
-    console.log(svgIsPresent);
     expect(svgIsPresent).toBe(true);
   });
-  */
   it('The dot id=pathway11202 sarts at 14, goes to 19 on mouseover and goes back to 14 on mouseout', async () => {
     const dot = element(by.id('pathway11202'));
     let rValue = await dot.getAttribute('r');
+    console.log(rValue);
     expect(rValue).toBe('14');
     await browser.actions().mouseMove(dot).perform();
     rValue = await dot.getAttribute('r');
