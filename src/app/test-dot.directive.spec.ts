2d62caac220bc158c7218bb6c59dbb289b8ebf68
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-05 14:10:34 -0600

    📅  | Fri Aug  5 14:10:34 MDT 2022

--- src/app/test-dot.directive.spec.ts
+++ src/app/test-dot.directive.spec.ts
@@ -4,21 +4,5 @@ import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('TestDotDirective', () => {
-  let fixture;
-  let dotElement;
-  beforeAll(async () => {
-    fixture = TestBed.configureTestingModule({
-      declarations: [AppComponent, TwoWayPlotDirective],
-    }).createComponent(AppComponent);
-    fixture.detectChanges();
-    dotElement = fixture.nativeElement.querySelector('#pathway11202');
-  });
-  afterAll(() => (fixture = null));
-  it('should create an instance', () => {
-    dotElement.addEventListener('mouseover', (event) => {
-      console.log('MOUSE OVER EVENT', event.target);
-    });
-    console.log(dotElement);
-    dotElement.dispatchEvent(new Event('mouseover'));
-  });
+  puppeteer;
 });
