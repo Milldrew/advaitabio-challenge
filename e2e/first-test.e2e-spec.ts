74cde5b1e9ed382acbd16e140c3dae78e58738c9
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-05 15:23:32 -0600

    📅  | Fri Aug  5 15:23:32 MDT 2022

--- e2e/first-test.e2e-spec.ts
+++ e2e/first-test.e2e-spec.ts
@@ -2,7 +2,19 @@ import { browser, by, element } from 'protractor';
 
 // Listing 8.4
 describe('our first Protractor test', () => {
-  it('should load a page and verify the url', async () => {
-    expect('hello').toEqual('hello');
+  beforeEach(async () => {
+    await browser.get('');
+    await browser.waitForAngular();
+  });
+  it('App is runningn on localhost:4200', async () => {
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
+    const title = await browser.getTitle();
+  });
+  it('The plot graph instantiates', async () => {
+    //const h1Ele = await element(by.name('h1')).getText();
+    //console.log(h1Ele);
+    const dotElement = element(by.id('pathway11202'));
+    const rValue = await dotElement.getAttribute('r');
+    console.log(rValue);
   });
 });
