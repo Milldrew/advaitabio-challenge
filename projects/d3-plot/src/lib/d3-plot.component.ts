6a7e4f57ad04b37e59b09105946895d3e49cabaf
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-09 12:57:39 -0600

    📅  | Tue Aug  9 12:57:39 MDT 2022

--- projects/d3-plot/src/lib/d3-plot.component.ts
+++ projects/d3-plot/src/lib/d3-plot.component.ts
@@ -1,53 +1,21 @@
+import * as data from './data.json';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-d3-plot',
-  template: `
-    <div
-      id="twoWayPlot"
-      class="plot"
-      [data]="data"
-      twoWayPlot
-      correction=""
-      l
-      sigThr="thr"
-      [disableSelection]="true"
-      axisXLabel="pORA"
-      axisYLabel="pAcc"
-      [numTicks]="35"
-      sigLabel="pComb"
-      itemLabel="pName"
-      itemSize="path_size"
-      itemId="pathID"
-      item="pathway"
-      itemScale="log"
-      (nextStateFn)="handleDotClick($event)"
-    ></div>
-    <div
-      id="twoWayPlotDisableSelectionFalse"
-      class="plot"
-      [data]="data"
-      twoWayPlot
-      correction=""
-      l
-      sigThr="thr"
-      [disableSelection]="false"
-      axisXLabel="pORA"
-      axisYLabel="pAcc"
-      [numTicks]="35"
-      sigLabel="pComb"
-      itemLabel="pName"
-      itemSize="path_size"
-      itemId="pathID"
-      item="pathway"
-      itemScale="log"
-      (nextStateFn)="handleDotClick($event)"
-    ></div>
-  `,
+  templateUrl: './d3-plot.component.html',
   styles: [],
 })
 export class D3PlotComponent implements OnInit {
-  constructor() {}
+  data;
+  constructor() {
+    this.data = Array.from(data);
+  }
 
+  clickDotValue = null;
+  handleDotClick(event) {
+    this.clickDotValue = event;
+    console.log(this.clickDotValue, 'click dot event value');
+  }
   ngOnInit(): void {}
 }
--- projects/d3-plot/src/lib/two-way-plot.directive.spec.ts
+++ projects/d3-plot/src/lib/two-way-plot.directive.spec.ts
@@ -2,12 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { D3PlotComponent } from './d3-plot.component';
 import { TwoWayPlotDirective } from './two-way-plot.directive';
 describe('TwoWayPlotDirective', () => {
-  let fixture: ComponentFixture<AppComponent>;
+  let fixture: ComponentFixture<D3PlotComponent>;
   let dotElement = null;
   beforeEach(async () => {
     fixture = TestBed.configureTestingModule({
-      declarations: [AppComponent, TwoWayPlotDirective],
-    }).createComponent(AppComponent);
+      declarations: [D3PlotComponent, TwoWayPlotDirective],
+    }).createComponent(D3PlotComponent);
     fixture.detectChanges();
   });
   it('should create an instance', () => {
