2f29d78f1c2b8d141296298059ff5cdce06faed4
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-09 15:45:31 -0600

    📅  | Tue Aug  9 15:45:31 MDT 2022

--- projects/de-plot-backup/src/lib/two-way-plot.directive.spec.ts
+++
@@ -1,7 +0,0 @@
-//import { TwoWayPlotDirective } from './two-way-plot.directive';
-describe('TwoWayPlotDirective', () => {
-  it('should create an instance', () => {
-    //const directive = new TwoWayPlotDirective();
-    expect('hello').toBeTruthy('hello');
-  });
-});
--- projects/de-plot-backup/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of d3-plot
- */
-
-export * from './lib/two-way-plot.directive';
-export * from './lib/d3-plot.module';
